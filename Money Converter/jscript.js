ce74d9b56aa72bed8067276581fca9b00e02f7db
Darnell Lockhart <107422361+darnell0211@users.noreply.github.com>
2024-01-31 14:53:31 -0500

    Add files via upload

---
+++ Money Converter/jscript.js
@@ -0,0 +1,49 @@
+const fromCur = document.querySelector(".from select");
+const toCur = document.querySelector(".to select");
+const getBtn = document.querySelector("form button");
+const exIcon = document.querySelector("form .reverse");
+const amount = document.querySelector("form input");
+const exRateTxt = document.querySelector("form .result");
+
+
+[fromCur, toCur].forEach((select, i) => {
+    for (let curCode in Country_List) {
+        const selected = (i === 0 && curCode === "USD") || (i === 1 && curCode === "GBP") ? "selected" : "";
+        select.insertAdjacentHTML("beforeend", `<option value="${curCode}" ${selected}>${curCode}</option>`);
+    }
+    select.addEventListener("change", () => {
+        const code = select.value;
+        const imgTag = select.parentElement.querySelector("img");
+    });
+});
+
+
+async function getExchangeRate() {
+    const amountVal = amount.value || 1;
+    exRateTxt.innerText = "Loading....";
+    try {
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/26dc88c3546b7559d2311d70/latest/${fromCur.value}`);
+        const result = await response.json();
+        const exchangeRate = result.conversion_rates[toCur.value];
+        const totalExRate = (amountVal * exchangeRate).toFixed(2);
+        exRateTxt.innerText = `${amountVal} ${fromCur.value} = ${totalExRate} ${toCur.value}`;
+    } catch (error) {
+        exRateTxt.innerText = "Error! Please Check Api Key.";
+    }
+}
+
+
+window.addEventListener("load", getExchangeRate);
+getBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    getExchangeRate();
+});
+
+exIcon.addEventListener("click", () => {
+    [fromCur.value, toCur.value] = [toCur.value, fromCur.value];
+    [fromCur, toCur].forEach((select) => {
+        const code = select.value;
+        const imgTag = select.parentElement.querySelector("img");
+    });
+    getExchangeRate();
+});
